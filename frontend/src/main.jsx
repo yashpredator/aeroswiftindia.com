e7e77dbb701f6dbe7ae261c4690d5e211c1cfe1c
yashpredator <yashrajsrivastava555@gmail.com>
2025-06-01 03:03:33 +0530

    patched

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import "slick-carousel/slick/slick-theme.css"; // Move this from SCSS to here
+
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./index.css"
 import store from "./redux/store/store.js";
